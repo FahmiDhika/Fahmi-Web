2f4e2f72d74c105f13532c448d5b97e80606ef71
FahmiDhika <fahmidhika810@gmail.com>
2024-11-04 22:37:01 +0700

    Navbar Done

---
+++ app/components/navbarButton.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const buttonNavbar = () => {
+    return(
+        <>
+            <button className=""><Link href="#profile">Profile</Link></button>
+        </>
+    )
+}
+
+export default buttonNavbar
